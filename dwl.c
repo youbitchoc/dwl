20b43b72852a4a3a1b3f67550b4447e80a38ebd3
choc <97781427+youbitchoc@users.noreply.github.com>
2022-07-25 17:09:17 +0800

    let zoom swap clients

--- dwl.c
+++ dwl.c
@@ -2900,23 +2900,30 @@ zoom(const Arg *arg)
 	if (!sel || !selmon || !selmon->lt[selmon->sellt]->arrange || sel->isfloating)
 		return;
 
-	/* Search for the first tiled window that is not sel, marking sel as
-	 * NULL if we pass it along the way */
+	/* Search for the first tiled window */
 	wl_list_for_each(c, &clients, link)
-		if (VISIBLEON(c, selmon) && !c->isfloating) {
-			if (c != sel)
+		if (VISIBLEON(c, selmon) && !c->isfloating)
 				break;
-			sel = NULL;
-		}
 
 	/* Return if no other tiled window was found */
 	if (&c->link == &clients)
 		return;
 
-	/* If we passed sel, move c to the front; otherwise, move sel to the
-	 * front */
-	if (!sel)
-		sel = c;
+	if (c == sel) {
+		/* sel is first tiled window
+		 * set sel to last tiled window */
+		wl_list_for_each_reverse(c, &clients, link)
+			if (VISIBLEON(c, selmon) && !c->isfloating)
+				break;
+ 		sel = c;
+	} else {
+		/* move c after sel */
+ 		wl_list_remove(&c->link);
+		wl_list_insert(&sel->link, &c->link);
+ 	}
+
+	/* move sel to front */
+
 	wl_list_remove(&sel->link);
 	wl_list_insert(&clients, &sel->link);
 
